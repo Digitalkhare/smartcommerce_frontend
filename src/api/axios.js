e2a5aeff119da3db36a6a6e62833685ff4312ea1
Osomokhare <kharealiu@gmail.com>
2025-06-05 20:27:01 +0100

    Working deployment version 3

--- src/api/axios.js
+++ src/api/axios.js
@@ -27,7 +27,7 @@ import axios from "axios";
 import { redirectToLogin } from "./navigation";
 
 const instance = axios.create({
-  baseURL: "https://smartcommerce-backend-f3aa4e6e4d32.herokuapp.com",
+  baseURL: "https://smartcommerce-backend-f3aa4e6e4d32.herokuapp.com/api",
   withCredentials: true,
 });
 
